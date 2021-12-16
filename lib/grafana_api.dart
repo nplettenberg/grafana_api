d5e7f7ac8f0ba2d236ed26f4c507e6823f551125
Nino Plettenberg <nino@plettenberge.de>
2021-12-16 21:01:10 +0100

    ADD barrel file

--- lib/grafana_api.dart
+++ lib/grafana_api.dart
@@ -1,1 +1,7 @@
 library grafana_api;
+
+export 'api/dashboard.dart';
+export 'api/data/dashboard/dashboard_response.dart';
+export 'client/abstract_grafana_client.dart';
+export 'client/grafana_client.dart';
+export 'grafana_api_base.dart';
